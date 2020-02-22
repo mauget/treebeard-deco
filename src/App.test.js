8df9087f9a50a79588382adf7d8103c32425f6d2
mauget <mauget@gmail.com>
2020-02-21 21:38:47 -0500

    Fix tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('main app heading', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const h1Text = getByText(/Tree View/i);
+  expect(h1Text).toBeInTheDocument();
 });
