54c69ee4b0426298542e4c0972799d94eefb3e4d
mauget <mauget@gmail.com>
2020-02-22 22:05:35 -0500

    Disable lint on this.renderToggle()

--- src/CustomContainer.js
+++ src/CustomContainer.js
@@ -4,18 +4,14 @@ import {decorators} from "react-treebeard";
 
 export default class CustomContainer extends decorators.Container {
 
-    renderToggle = () => super.renderToggle();
-
     render() {
         const {style, decorators, terminal, onClick, node} = this.props;
 
-        return (
-            <div style={style.container[0]}>
-                <decorators.Header node={node} style={style.header}/>
-                <span onClick={onClick} style={{float: "left", cursor: "pointer"}}>
-                    {terminal ? null : super.renderToggle()}
+        return <div style={style.container[0]}>
+            <decorators.Header node={node} style={style.header}/>
+            <span onClick={onClick} style={{float: "left", cursor: "pointer"}}>
+                    {terminal ? null : this.renderToggle()}
                 </span>
-            </div>
-        );
+        </div>;
     }
 }
