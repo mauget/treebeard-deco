dda7a624322b7e8c28867696f03590a68975ec27
mauget <mauget@gmail.com>
2020-02-25 08:32:15 -0500

    Highighted node on mouseover

--- src/CustomHeader.js
+++ src/CustomHeader.js
@@ -1,32 +1,56 @@
-import React from "react";
+import React, {useState} from "react";
 import nodeCallback from "./nodeCallback";
+
+const NotSelected = {backgroundColor: "#282c34", color: "#d0d0d0"};
+const Selected = {backgroundColor: "#c0c0c0", color: "black"};
 
 export default function CustomHeader(props) {
 
     const {node, style} = props;
     const displayName = node.name;
-    const thisStyle = style.base;
+    const headerStyle = style.base;
+
+    const [selected, setSelected] = useState(NotSelected);
 
     const handleHeaderClick = ev => {
         ev.preventDefault();
+        setSelected(Selected);
 
-        // Invoke node-select processing
-        nodeCallback(ev.target.innerText);
+        // Invoke imported node-select callback
+        nodeCallback(displayName);
     };
 
     const handleContextMenu = ev => {
         ev.preventDefault();
+
+        // Context menu processing placeholder
         alert(`"${displayName}" right-clicked`);
+    };
+
+    const handleMouseEnter = ev => {
+        ev.preventDefault();
+        setSelected(Selected);
+    };
+
+    const handleMouseLeave = ev => {
+        ev.preventDefault();
+        setSelected(NotSelected);
     };
 
     return (
         <div
             className={"App-header"}
-            style={thisStyle}
+            style={headerStyle}
             onClick={handleHeaderClick}
-            onContextMenu={handleContextMenu}>
+            onContextMenu={handleContextMenu}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+        >
 
-            <div className={"App-header-name"}>
+            <div
+                style={selected}
+                className={"App-header-name"}
+            >
                 {displayName}
             </div>
         </div>
