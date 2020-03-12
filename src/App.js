9d52a1e8501727dd42642882e6d3b2ba352f5032
mauget <mauget@gmail.com>
2020-03-11 20:58:32 -0400

    Log data and JSON of data

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import TreeView from "./TreeView";
 import {data} from "./data";
 
 function App() {
+    console.log(JSON.stringify(data));
+    console.log(data);
     return <div className="App">
         <h1>Tree View via Decorators</h1>
         <hr/>
