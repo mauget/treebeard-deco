975b22ac486d9a3ca01582eb0e69a7b7afdf677e
mauget <mauget@gmail.com>
2020-03-15 20:48:09 -0400

    Test verify: varying case of name 1st char: sort children

--- src/treeModel.test.js
+++ src/treeModel.test.js
@@ -6,5 +6,5 @@ it('treeModel(getScenarios()) creates expected Treebeard data', () => {
     const dataTb = treeModel(getScenarios());
 
     expect(dataTb.children[0].children.length).toBe(4);
-    expect(dataTb.children[0].children[0].name).toBe('demo');
+    expect(dataTb.children[0].children[0].name).toBe('Demo');
 });
