4cd4476c76b7ad217edeecef4ca4685b4893e6b2
mauget <mauget@gmail.com>
2020-03-26 19:19:34 -0400

    Thin blue border on selection box

--- src/CustomHeader.js
+++ src/CustomHeader.js
@@ -2,56 +2,46 @@ import React, {useState} from "react";
 import nodeCallback from "./nodeCallback";
 
 const NotSelected = {backgroundColor: "#282c34", color: "#d0d0d0"};
-const Selected = {backgroundColor: "#c0c0c0", color: "black"};
+const Selected = {backgroundColor: "#c0c0c0", color: "black", border: "1px solid blue", paddingLeft: "2px", paddingRight: "2px"};
 
 export default function CustomHeader(props) {
 
-    const {node, style} = props;
+    const { node, style } = props;
     const displayName = node.name;
     const headerStyle = style.base;
 
-    const [selected, setSelected] = useState(NotSelected);
+    const [ selected, setSelected ] = useState(NotSelected);
 
     const handleHeaderClick = ev => {
         ev.preventDefault();
-        setSelected(Selected);
 
         // Invoke imported node-select callback
-        nodeCallback(displayName);
+  ////      nodeCallback(displayName);
+        setSelected(Selected);
     };
 
     const handleContextMenu = ev => {
         ev.preventDefault();
 
         // Context menu processing placeholder
-        alert(`"${displayName}" right-clicked. Replace this by a context popup.`);
-    };
-
-    const handleMouseEnter = ev => {
-        ev.preventDefault();
+  ////      alert(`"${displayName}" right-clicked. Replace this by a context popup.`);
         setSelected(Selected);
     };
 
-    const handleMouseLeave = ev => {
-        ev.preventDefault();
-        setSelected(NotSelected);
-    };
 
     return (
         <div
-            className={"App-header"}
-            style={headerStyle}
-            onClick={handleHeaderClick}
-            onContextMenu={handleContextMenu}
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
+            className={ "App-header" }
+            style={ headerStyle }
+            onClick={ handleHeaderClick }
+            onContextMenu={ handleContextMenu }
         >
 
             <div
-                style={selected}
-                className={"App-header-name"}
+                style={ selected }
+                className={ "App-header-name" }
             >
-                {displayName}
+                { displayName }
             </div>
         </div>
     );
