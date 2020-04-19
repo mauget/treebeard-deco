9848827cddadfe5726d5967976acd4d021021237
mauget <mauget@gmail.com>
2020-04-19 19:20:20 -0400

    yarn test defaults to all src with coverage

--- src/TreeView.test.js
+++ src/TreeView.test.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Treebeard} from 'react-treebeard';
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Treebeard data={{}}/>, div);
-    ReactDOM.unmountComponentAtNode(div);
-});
+describe('TreeView', () => {
 
+    test('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Treebeard data={{}}/>, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+});
