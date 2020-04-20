1ef48679f93ef354dae8615d7cfef4b9d693b22a
Louis Mauget <lmauget@ara.com>
2020-04-20 08:36:52 -0400

    Restore render import in index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {render} from 'react-dom';
 import {Provider} from "react-redux";
 import {createStore, applyMiddleware} from "redux";
 import {composeWithDevTools} from "redux-devtools-extension";
